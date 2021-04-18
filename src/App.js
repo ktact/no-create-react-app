e093e8ad551b63200e4a22f118b1b72644b2f2d6
K Tact <ktact.dev@gmail.com>
2021-04-18 11:41:15 +0900

    async/awaitが使用できるように修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
-  return(
-    <div>Hello, World</div>
+  const [data, setData] = useState({ hits: []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://hn.algolia.com/api/v1/search?query=redux',
+      );
+
+      console.log(result.data);
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
